234d910a8be31f78e1e5494b5017fc628b6618ea
DavidIsrael <davidisrael.rj@gmail.com>
2025-05-27 20:35:15 -0300

    Menu

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,10 @@
+import Menu from "@/template/menu";
 
 export default function Home() {
   return (
     <div className={`flex flex-row justify-center p-4 `}>
       <div className="text-9xl font-bold">
+      <Menu/>
         Site de Bolos
       </div>
     </div>
