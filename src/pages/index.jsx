775673a5dbd22901d57e50c2039049de6d24fd0d
DavidIsrael <davidisrael.rj@gmail.com>
2025-05-27 20:40:37 -0300

    sideBar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,11 @@
 import Menu from "@/template/menu";
+import SiderBar from "@/template/sideBar";
 
 export default function Home() {
   return (
     <div className={`flex flex-row justify-center p-4 `}>
-      <div className="text-9xl font-bold">
       <Menu/>
-        Site de Bolos
-      </div>
+      <SiderBar/>
     </div>
   );
 }
---
+++ src/template/sideBar.jsx
@@ -0,0 +1,7 @@
+export default function SiderBar() {
+    return (
+        <div className="text-9xl font-bold">
+            Site de Bolos
+        </div>
+    )
+}
