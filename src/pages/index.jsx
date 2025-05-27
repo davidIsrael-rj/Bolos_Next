bb7159bd8aa18f148be1e3033d18d5898d03c614
DavidIsrael <davidisrael.rj@gmail.com>
2025-05-26 22:48:26 -0300

    Modificando cor de fundo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,10 @@
 
 export default function Home() {
   return (
-    <div>
-      Site de Bolos
+    <div className={`flex flex-row justify-center p-4 `}>
+      <div className="text-9xl font-bold">
+        Site de Bolos
+      </div>
     </div>
   );
 }
