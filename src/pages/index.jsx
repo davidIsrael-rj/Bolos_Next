b567b1466c0fb6108ed620cd7f0c1fffc5db006d
DavidIsrael <davidisrael.rj@gmail.com>
2025-06-20 22:26:51 -0300

    Ajustando o layout da tela

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import Header from "@/template/header";
+import Menu from "@/template/menu";
 import SiderBar from "@/template/sideBar";
 import TopBar from "@/template/topBar";
 
 export default function Home() {
   return (
-    <div className={`flex flex-row justify-center p-4 `}>
+    // <div className={`flex flex-row justify-center p-4 `}>
+    <div className={"area"}>
       <SiderBar/>
       <TopBar/>
       <Header/>
