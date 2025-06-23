7f57d5ac842ccff918499b8f5b3277643c427a8d
DavidIsrael <davidisrael.rj@gmail.com>
2025-06-23 20:08:18 -0300

    Main

---
+++ src/components/template/Main.jsx
@@ -0,0 +1,11 @@
+import Header from './Header'
+import './Main.css'
+import React from 'react'
+
+export default props =>
+    <>
+        <Header />
+        <main className="content">
+            Conteúdo
+        </main>
+    </>
