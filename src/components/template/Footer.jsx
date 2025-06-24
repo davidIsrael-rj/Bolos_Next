971720f1cf8d1832316897fa2986960e947c627e
DavidIsrael <davidisrael.rj@gmail.com>
2025-06-24 17:25:26 -0300

    Inserindo estilo e um texto no footer

--- src/components/template/Footer.jsx
+++ src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com  <i className='fa fa-heart text-danger'></i> por
+            <strong> Dav<span className='text-danger'>i</span>d</strong>
+        </span>
     </footer>
