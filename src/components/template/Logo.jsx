6342fa1eca0af27d34ef5b061d26161ddc6302a1
DavidIsrael <davidisrael.rj@gmail.com>
2025-06-24 16:30:46 -0300

    Inserindo uma imagem no logo

--- src/components/template/Logo.jsx
+++ src/components/template/Logo.jsx
@@ -1,7 +1,12 @@
 import './Logo.css'
+import logo from '../../assets/imgs/bolo_rosa.jpg'
 import React from 'react'
+import Image from 'next/image'
+
 
 export default props => 
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <Image src={logo} alt="logo" />
+        </a>
     </aside>
