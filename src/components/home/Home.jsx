f663dece6a807f30255c87dd5c3132a58675d537
DavidIsrael <davidisrael.rj@gmail.com>
2025-06-29 21:02:50 -0300

    Home

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Bolos feitos com carinhos" >
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Loja de Bolos</p>
+    </Main>
--- src/main/AppBolo.jsx
+++ src/main/AppBolo.jsx
@@ -3,19 +3,14 @@ import 'font-awesome/css/font-awesome.min.css'
 import './AppBolo.css'
 import Footer from "@/components/template/Footer";
 import Logo from "@/components/template/Logo";
-import Main from "@/components/template/Main";
 import Nav from "@/components/template/Nav";
 import React from "react";
+import Home from '@/components/home/Home';
 
 export default props =>
     <div className="appBolo">
         <Logo />
         <Nav />
-        <Main icon="home" title="Início"
-            subtitle="Bolos feitos com carinhos" >
-              <div className="display-4">Bem Vindo!</div> 
-              <hr />
-              <p className="mb-0">Loja de Bolos</p> 
-        </Main>
+        <Home/> 
         <Footer />
     </div>
