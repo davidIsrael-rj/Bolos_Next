7d127bc72f05b04e8168331b80bac8cac63f85ae
DavidIsrael <davidisrael.rj@gmail.com>
2025-07-01 21:40:46 -0300

    Função clear

--- src/components/template/Logo.jsx
+++ src/components/template/Logo.jsx
@@ -2,11 +2,12 @@ import './Logo.css'
 import logo from '../../assets/imgs/bolo_rosa.jpg'
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 
 export default props => 
     <aside className="logo">
-        <a href="/" className="logo">
+        <Link href="/" className="logo">
             <Image src={logo} alt="logo" />
-        </a>
+        </Link>
     </aside>
--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -7,8 +7,22 @@ const headerProps = {
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!!!'
 }
 
+const baseUrl = 'http://localhost:3001/users'
+
+const initialState = {
+    user: { name: '', email: '' },
+    list: []
+}
+
 export default class UserCrud extends Component {
-    render(){
+
+    state = {...initialState}
+
+    clear(){
+        this.setState({user: initialState.user})
+    }
+
+    render() {
         return (
             <Main {...headerProps}>
                 Cadastro de Usuário
