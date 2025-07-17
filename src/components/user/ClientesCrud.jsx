91b1f20a909e172a317876b96eb08e35d1dc7638
DavidIsrael <davidisrael.rj@gmail.com>
2025-07-17 20:43:58 -0300

    InitialState Clientes

--- src/components/user/ClientesCrud.jsx
+++ src/components/user/ClientesCrud.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
 import Main from "../template/Main";
 
-const headerProps ={
+const headerProps = {
     icon: 'address-card',
     title: 'Clientes',
-    subtitle:'Cadastro de Clientes : Incluir, Listar, Alterar e Excluir!!!'
+    subtitle: 'Cadastro de Clientes : Incluir, Listar, Alterar e Excluir!!!'
 }
-export default class ClientesCrud extends Component{
-    render(){
+
+const baseUrl = 'http://localhost:3001/clientes'
+
+const initialState = {
+    clientes: {
+        name: "",
+        endereco: "",
+        numero: "",
+        bairro: "",
+        municipio: "",
+        estado: ""
+    },
+    list: []
+}
+
+export default class ClientesCrud extends Component {
+
+    state = {...initialState}
+
+
+    render() {
         return (
             <Main {...headerProps}>
                 <h1>Clientes</h1>
