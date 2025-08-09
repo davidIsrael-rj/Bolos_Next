079ce2f9ac65c21cb238c50d5fb742fae66c66d2
DavidIsrael <davidisrael.rj@gmail.com>
2025-08-09 20:10:24 -0300

    Carrossel

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Main from "../template/Main";
 import Image from "next/image";
 import logo from '../../assets/imgs/logo.png'
+import bolo1 from '../../assets/imgs/bolo_rosa1.jpg'
+import bolo2 from '../../assets/imgs/bolo_rosa2.jpg'
+import bolo3 from '../../assets/imgs/bolo_rosa3.jpg'
+import bolo4 from '../../assets/imgs/bolo_rosa4.jpg'
 import fone from '../../assets/imgs/fone.png'
 import email from '../../assets/imgs/email.png'
 import styles from './Home.module.css'
@@ -9,6 +13,22 @@ import Link from "next/link";
 
 export default function Home(props) {
 
+
+    const [indiceAtual, setIndiceAtual] = useState(0);
+    const imagens = [
+        { src: bolo1, alt: 'Imagem 1' },
+        { src: bolo2, alt: 'Imagem 2' },
+        { src: bolo3, alt: 'Imagem 3' },
+        { src: bolo4, alt: 'Imagem 4' },
+    ];
+
+    const handleAnterior = () => {
+        setIndiceAtual((indiceAtual - 1 + imagens.length) % imagens.length);
+    };
+
+    const handleProximo = () => {
+        setIndiceAtual((indiceAtual + 1) % imagens.length);
+    };
     return (
         <Main icon="home" title="Início"
             subtitle="Bolos feitos com carinhos" >
@@ -133,6 +153,26 @@ export default function Home(props) {
                         </div>
                     </div>
                 </section>
+
+                {/* Carrossel */}
+                <div className={styles.carrossel}>
+                    <div className={styles.carrosselItens}>
+                        {imagens.map((imagem, indice) => (
+                            <div key={indice} className={`${styles.carrosselItem} ${indice === indiceAtual ? styles.active : ''}`} >
+                                <Image className={styles.carrosselItemImg} src={imagem.src} alt={imagem.alt} />
+                            </div>
+                        ))}
+                    </div>
+                    <button className={styles.carrosselAnterior} onClick={handleAnterior}> &#10094; </button>
+                    <button className={styles.carrosselProximo} onClick={handleProximo}> &#10095; </button>
+                    <div className={styles.carrosselPaginacao}>
+                        {imagens.map((imagem, indice) => (
+                            <span key={indice} className={`${styles.carrosselPagina} ${indice === indiceAtual ? styles.active : ''}`} onClick={() => setIndiceAtual(indice)} />
+                        ))}
+                    </div>
+                </div>
+
+
                 <section id="contatos">
                     <div className={`container-fluid text-center ${styles.margin} ${styles.contatos}`}>
                         <a className="nav-link" href="#" onclick="window.scrollTo({ top: 0, behavior: 'smooth' }); return false;">
